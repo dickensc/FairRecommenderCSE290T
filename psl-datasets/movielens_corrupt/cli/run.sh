48243837ea3ee1d59cc00572122ddd9a56360607
Oasys Okubo <oasysokubo@gmail.com>
2021-05-13 10:47:28 -0700

    corrupted sets

---
+++ psl-datasets/movielens_corrupt/cli/run.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+# Options can also be passed on the command line.
+# These options are blind-passed to the CLI.
+# Ex: ./run.sh -D log4j.threshold=DEBUG
+
+readonly PSL_VERSION='2.3.0-SNAPSHOT'
+readonly JAR_PATH="../../psl_resources/psl-cli-${PSL_VERSION}.jar" # "./psl-cli-${PSL_VERSION}.jar"
+readonly FETCH_DATA_SCRIPT='../data/fetchData.sh'
+readonly BASE_NAME='movielens'
+readonly POSTGRES_DB='psl'
+readonly STANDARD_PSL_OPTIONS="--postgres ${POSTGRES_DB}"
+
+readonly ADDITIONAL_PSL_OPTIONS='--int-ids --postgres psl -D log4j.threshold=TRACE'
+readonly ADDITIONAL_LEARN_OPTIONS='--learn org.linqs.psl.application.learning.weight.bayesian.GaussianProcessPrior -D random.seed=4 -D admmreasoner.initialconsensusvalue=ZERO -D gppker.reldep=1 -D gpp.explore=1 -D gpp.maxiterations=25 -D gppker.space=SS -D gpp.initialweightstd=0.5 -D gpp.initialweightvalue=0.5 -D log4j.threshold=TRACE  -D weightlearning.evaluator=org.linqs.psl.evaluation.statistics.ContinuousEvaluator '
+readonly ADDITIONAL_EVAL_OPTIONS='--infer SGDInference -D sgd.maxiterations=500 --eval org.linqs.psl.evaluation.statistics.ContinuousEvaluator'
+
+readonly JAVA_MEM_GB=24
+
+function main() {
+   trap exit SIGINT
+
+   # Get the data
+   # getData
+
+   # Make sure we can run PSL.
+   check_requirements
+   # fetch_psl
+
+   # Run PSL
+   runWeightLearning "$@"
+   runEvaluation "$@"
+}
+
+function getData() {
+   pushd . > /dev/null
+
+   cd "$(dirname $FETCH_DATA_SCRIPT)"
+   bash "$(basename $FETCH_DATA_SCRIPT)"
+
+   popd > /dev/null
+}
+
+function runWeightLearning() {
+   echo "Running PSL Weight Learning"
+
+   java -Xmx${JAVA_MEM_GB}G -Xms${JAVA_MEM_GB}G -jar "${JAR_PATH}" --model "${BASE_NAME}.psl" --data "${BASE_NAME}-learn.data" ${ADDITIONAL_LEARN_OPTIONS} ${ADDITIONAL_PSL_OPTIONS} "$@"
+   if [[ "$?" -ne 0 ]]; then
+      echo 'ERROR: Failed to run weight learning'
+      exit 60
+   fi
+}
+
+function runEvaluation() {
+   echo "Running PSL Inference"
+
+   java -Xmx${JAVA_MEM_GB}G -Xms${JAVA_MEM_GB}G -jar "${JAR_PATH}" --model "${BASE_NAME}-learned.psl" --data "${BASE_NAME}-eval.data" --output inferred-predicates ${ADDITIONAL_EVAL_OPTIONS} ${ADDITIONAL_PSL_OPTIONS} "$@"
+   if [[ "$?" -ne 0 ]]; then
+      echo 'ERROR: Failed to run infernce'
+      exit 70
+   fi
+}
+
+function check_requirements() {
+   local hasWget
+   local hasCurl
+
+   type wget > /dev/null 2> /dev/null
+   hasWget=$?
+
+   type curl > /dev/null 2> /dev/null
+   hasCurl=$?
+
+   if [[ "${hasWget}" -ne 0 ]] && [[ "${hasCurl}" -ne 0 ]]; then
+      echo 'ERROR: wget or curl required to download dataset'
+      exit 10
+   fi
+
+   type java > /dev/null 2> /dev/null
+   if [[ "$?" -ne 0 ]]; then
+      echo 'ERROR: java required to run project'
+      exit 13
+   fi
+}
+
+function get_fetch_command() {
+   type curl > /dev/null 2> /dev/null
+   if [[ "$?" -eq 0 ]]; then
+      echo "curl -o"
+      return
+   fi
+
+   type wget > /dev/null 2> /dev/null
+   if [[ "$?" -eq 0 ]]; then
+      echo "wget -O"
+      return
+   fi
+
+   echo 'ERROR: wget or curl not found'
+   exit 20
+}
+
+function fetch_file() {
+   local url=$1
+   local path=$2
+   local name=$3
+
+   if [[ -e "${path}" ]]; then
+      echo "${name} file found cached, skipping download."
+      return
+   fi
+
+   echo "Downloading ${name} file located at: '${url}'."
+   `get_fetch_command` "${path}" "${url}"
+   if [[ "$?" -ne 0 ]]; then
+      echo "ERROR: Failed to download ${name} file"
+      exit 30
+   fi
+}
+
+# Fetch the jar from a remote or local location and put it in this directory.
+# Snapshots are fetched from the local maven repo and other builds are fetched remotely.
+function fetch_psl() {
+   if [[ $PSL_VERSION == *'SNAPSHOT'* ]]; then
+      local snapshotJARPath="$HOME/.m2/repository/org/linqs/psl-cli/${PSL_VERSION}/psl-cli-${PSL_VERSION}.jar"
+      cp "${snapshotJARPath}" "${JAR_PATH}"
+   else
+      local remoteJARURL="https://repo1.maven.org/maven2/org/linqs/psl-cli/${PSL_VERSION}/psl-cli-${PSL_VERSION}.jar"
+      fetch_file "${remoteJARURL}" "${JAR_PATH}" 'psl-jar'
+   fi
+}
+
+main "$@"

5f8945b4c29325d9b2cc514da0c7285959f9cfba
Charles Dickens <charlieadickens@gmail.com>
2021-06-03 07:58:09 -0700

    group1 group2

--- data_construction/predicate_constructors/fairness_predicates/group_2.py
+++ data_construction/predicate_constructors/fairness_predicates/group_2.py
@@ -9,6 +9,7 @@ def group_2(user_df, fold='0', phase='eval'):
     group_2(U)
     """
     group_member_df = user_df.loc[:, ['gender']]
-    group_member_df.loc[:, 'value'] = 1
-    write(group_member_df[group_member_df.gender == 'M'].value, 'group_2_obs', fold, phase)
-    write(group_member_df.value, 'group_2_targets_targets', fold, phase)
+    group_member_df.loc[:, 'value'] = 0
+    group_member_df[group_member_df.gender == 'M'].value = 1
+    write(group_member_df.value, 'group_2_obs', fold, phase)
+    write(group_member_df.loc[:, []], 'group_2_targets_targets', fold, phase)
